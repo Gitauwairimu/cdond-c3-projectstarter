5d72b4d05e2ebe20e5b2bac3eabfc13872fb1916
gitauwairimu <gitauwairimu@gmail.com>
2022-08-23 08:10:13 +0300

    terraform added for prometheus server

---
+++ prometheus-server-terraform/main.tf
@@ -0,0 +1,17 @@
+terrform {
+    required_providers {
+      aws = {
+        source = "hashicorp"/aws"
+        version = ">= 3.0"
+        }
+    } 
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "prometheus" {
+    ami = "ami-011899242bb902164" #ubuntu 20.04 LTS // us-east-1
+    instance_type = "t2.micro"
+}
