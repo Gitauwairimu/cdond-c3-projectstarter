3a185c85b37586b9e1950bcbfcab69e3b0e2e76e
gitauwairimu <gitauwairimu@gmail.com>
2022-08-23 12:53:10 +0300

    rm part of provider block

--- prometheus-server-terraform/main.tf
+++ prometheus-server-terraform/main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAU5346P2ZZJSLKTNE"
-    secret_key = "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"
     # access_key = "$access_key"
     # secret_key = "$secret_key"
 }
